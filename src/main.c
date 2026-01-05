713b1aaf173ea0b6aa97c4224dff6fe5c4c0c71a
TimMeyerfeldt <tim.meyerfeldt@haw-hamburg.de>
2026-01-05 08:36:33 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -192,7 +192,7 @@ int main(int argc, char** argv) {
 
     strcpy(ownName, chat_name);
 
-    start_sender();
+    start_sender(); 
     start_server(local_port, chat_name);
 
     if (target_host[0] != '\0') {
